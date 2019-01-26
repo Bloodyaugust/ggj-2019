b0803c5e349ac5e1feb0aee5ff690b59a860ab39
Tara Timmerman <42617001+taratimmerman@users.noreply.github.com>
2019-01-26 12:56:00 -0700

    Initial player movement
    Horizontal

---
+++ Assets/Core/Scripts/Behaviors/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MovementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime * MovementSpeed;
+
+    }
+}
