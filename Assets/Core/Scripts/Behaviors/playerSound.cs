88a7d0e3986b8330655f7c6d57263b017fecf08b
AnneWellings <AnneWellings@users.noreply.github.com>
2019-01-26 23:51:53 -0700

    some of the sound stuff

---
+++ Assets/Core/Scripts/Behaviors/playerSound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class playerSound : MonoBehaviour
+{
+    public AudioClip engine1;
+    public AudioClip engine2;
+    public AudioClip backup;
+    public AudioClip honk;
+    public AudioClip collision;
+    AudioSource playerAudioSource;
+
+    Player _player;
+    PlayerController _playerController;
+
+    void Awake()
+    {
+        _playerController = GetComponent<PlayerController>();
+        _player = ReInput.players.GetPlayer(_playerController.PlayerIndex);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_player.GetAxis("Move"));
+    }
+}
