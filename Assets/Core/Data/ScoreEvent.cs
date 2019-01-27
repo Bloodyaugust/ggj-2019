b06ba4678b48d3fd40833880d0a5d5dbc40db269
greyson <greyson@l4digital.com>
2019-01-26 18:32:59 -0800

    Set up Rewired basic config. Event data structures now have constructors. PlayerController created and integrated with toolbox for relevant events. Player activity state and score functionality created.

--- Assets/Core/Data/ScoreEvent.cs
+++ Assets/Core/Data/ScoreEvent.cs
@@ -6,6 +6,11 @@ using UnityEngine.Events;
 public class ScoreData {
   public int Amount;
   public int PlayerIndex;
+
+  public ScoreData (int amount, int playerIndex) {
+    Amount = amount;
+    PlayerIndex = playerIndex;
+  }
 }
 
 [System.Serializable]
